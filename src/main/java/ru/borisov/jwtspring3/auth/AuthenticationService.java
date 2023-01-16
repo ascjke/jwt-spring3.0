57d07cbd9517141cfbd4aecac3656f10b7da3be2
Zakhar Borisov <zakhar.borisov@mail.ru>
2023-01-16 23:45:57 +0900

    Initial commit

---
+++ src/main/java/ru/borisov/jwtspring3/auth/AuthenticationService.java
@@ -0,0 +1,54 @@
+package ru.borisov.jwtspring3.auth;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ProblemDetail;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import ru.borisov.jwtspring3.config.JwtService;
+import ru.borisov.jwtspring3.user.Role;
+import ru.borisov.jwtspring3.user.User;
+import ru.borisov.jwtspring3.user.UserRepository;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService {
+
+    private final UserRepository repository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
+    private final AuthenticationManager authenticationManager;
+
+    public AuthenticationResponse register(RegisterRequest request) {
+        var user = User.builder()
+                .firstname(request.getFirstname())
+                .lastname(request.getLastname())
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .role(Role.USER)
+                .build();
+        repository.save(user);
+        var jwtToken = jwtService.generateToken(user);
+
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
+    }
+
+    public AuthenticationResponse authenticate(AuthenticationRequest request) {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getEmail(),
+                        request.getPassword()
+                )
+        );
+        var user = repository.findByEmail(request.getEmail())
+                .orElseThrow();
+        var jwtToken = jwtService.generateToken(user);
+
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
+    }
+}
---
+++ src/main/java/ru/borisov/jwtspring3/user/Role.java
@@ -0,0 +1,6 @@
+package ru.borisov.jwtspring3.user;
+
+public enum Role {
+    USER,
+    ADMIN
+}
